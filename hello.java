d97b2d413d1a1e5fa6949ec84255e155fe7cfcf4
kunjalmehta <45755486+kunjalmehta@users.noreply.github.com>
2018-12-10 17:51:12 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,5 @@
 public class Hello {
     public static void main(String args[]){
-        System.out.println("I'm dancing today in the rain with my dog");
+        System.out.println("I'm dancing today");
     }
 }
